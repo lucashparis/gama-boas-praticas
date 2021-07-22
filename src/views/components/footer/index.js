7d7ab5b7affdb7844bba1f6fb111db45714254f6
Lucas Paris <lucas@marknet.com.br>
2021-07-22 19:17:07 -0300

    First commit

---
+++ src/views/components/footer/index.js
@@ -0,0 +1,15 @@
+import Planet from '../../../assets/images/elements/planet.png';
+
+let footer = `
+    <div class="footer-contain">
+        <p>
+        Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+        Módulo de boas práticas.
+        </p>
+        <a href="#section-one">
+            <img src="${Planet}" alt="planet" width="auto" heigth="180px">
+        </a>    
+    </div>
+`
+
+export default footer;
